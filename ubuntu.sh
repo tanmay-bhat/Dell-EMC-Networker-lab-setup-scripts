9b64efcbe78776a23645754f8818c96ca2f49437
tanmay bhat <tanmaybhat24@gmail.com>
2021-01-21 19:10:25 +0000

    add new packages & change file name

---
+++ ubuntu.sh
@@ -0,0 +1,63 @@
+#!/bin/bash
+
+log=log_file.txt
+
+
+is_root()
+{
+        [[ $(id -u) -eq 0 ]];
+}
+
+if is_root; then
+        echo "yep, you are the Demi-God, go ahead please !"
+        
+else
+        echo "WARNING ! moving with  weakness!!"
+        exit 1
+fi
+
+for i in gcc make linux-headers-$(uname -r) liblzo2-dev mt-st mtx perl; do
+	apt install -y $i
+done
+		
+
+#1 crating user: mhvtl and group:mhvtl
+
+groupadd -r vtl >>$log
+useradd -r -c "VTL Account" -d /opt/mhvtl -g vtl vtl >>$log
+
+#2 creating reruired directories
+
+mkdir -p /opt/mhvtl
+mkdir -p /etc/mhvtl
+chown -Rf vtl:vtl /opt/mhvtl
+chown -Rf vtl:vtl /etc/mhvtl
+
+#3 downloading mtvl tar file from webiste
+
+cd
+mkdir src
+cd src
+wget --no-check-certificate 'https://drive.google.com/file/d/1kxlwcH9ldZZFC4_2c3AkVagVKYSoVFUe/view?usp=sharing' -O mhvtl.tar
+
+tar -xf mhvtl.tar
+
+cd kernel
+make&&make install
+
+cd ..
+make&&make install
+
+cd ..
+rm -rf src
+
+# adding  service  for startup and starting sevices
+
+update-ec.d mhvtl defaults
+service mhvtl start
+
+
+clear
+lsscsi -g | grep -E "mediumx|tape"
+
+
